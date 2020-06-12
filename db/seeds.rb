5731d0156082922b8a9f9c0b759d8ede72834e5c
jhell85 <jhell85@gmail.com>
2020-06-12 10:23:09 -0700

    add faker seeds to seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_quotes
+  end
+
+  def generate_animals
+    20.times do |i|
+      animal = Animal.create!(
+        type: Faker::Creature::Animal.name,
+        name: Faker::Name.first_name
+        age: Faker::Number.within(range: 1..10)
+      )
+      puts "Animal #{i}: Animal type is #{animal.type} their name is '#{animal.name}' and they are #{animal.age} years old."
+    end
+  end
+end
+
+Seed.begin
