ac98fa874dc48d8cc874125958328dffbbf8cf41
jhell85 <jhell85@gmail.com>
2020-06-12 09:12:00 -0700

    add controller routes

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,28 @@
+class AnimalsController < ApplicationController 
+
+  def index 
+    @animals = Animal.all
+    json_response(@animals)
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+    json_response(@animal)
+  end
+
+  def create
+    @animal = Animal.create(animal_params)
+    json_response(@animal)
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update(animal_params)
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+  end
+
+  end
