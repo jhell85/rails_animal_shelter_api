776f6f3a066b474c2117f745d32a793b562a0ce6
jhell85 <jhell85@gmail.com>
2020-06-25 05:56:12 -0700

    fixed issues with test all now passing

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 describe " post a animal route", :type => :request do
   
   before do
-    post '/animals', params: { :species => 'dog', :age => '5', name: => 'Wilber' }
+    post '/animals', params: { :species => 'dog', :age => '5', :name => 'Wilber' }
   end
 
   it 'returns the animals species' do
-    expect (JSON.pase(response.body)['species']).to eq('dog')
+    expect(JSON.parse(response.body)['species']).to eq('dog')
   end
 
   it 'returns the animals name' do
-    expect (JSON.pase(response.body)['name']).to eq('Wilber')
+    expect(JSON.parse(response.body)['name']).to eq('Wilber')
   end
 
   it 'returns the animals age' do
-    expect (JSON.pase(response.body)['age']).to eq('5')
+    expect(JSON.parse(response.body)['age']).to eq(5)
   end
   
 
